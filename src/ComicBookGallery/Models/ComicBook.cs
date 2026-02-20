23f7c26a5738e32358a31cdd33b7bd5f5e367014
devdaphnee2@gmail.com <devdaphnee2@gmail.com>
2026-02-20 01:43:47 +0100

    Updated comic book models

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,27 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public string Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + "#" + IssueNumber;
+            }
+
+            //series-title-issuesnumber.jpg
+
+            public string CoverImageFileName
+            {
+                 get
+                 {
+                    return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber +".jpg";
+                 }
+
+        }
+    }
+            
+
+        
     }
 }
